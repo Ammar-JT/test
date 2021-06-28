4a86ed9a2c912a03451b4e818d1dd58621a6a284
AmmarJT <AmmarTashk@gmail.com>
2021-06-28 19:54:07 +0300

    just made PagesController@pdf() method, which can read a pdf file from the private path '/test/storage/file.pdf'

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function pdf() {
+        $publicPath = getcwd();
+        $rootPath = substr($publicPath, 0, strpos($publicPath, "\public"));
+        $path = $rootPath .'\storage\file.pdf';
+        $type = "application/pdf";
+        header('Content-Type: '. $type);
+        header('Content-Length: '.filesize($path));
+
+        readfile($path);
+        die;
+     }
+    public function pdfFromPublicFile(){
+
+        $currentPath = getcwd();
+        //dd($currentPath);
+        $path = $currentPath . '/file.pdf';
+        $type = "application/pdf";
+        header('Content-Type: '. $type);
+       // header('Content-Disposition: attachment; filename="file.pdf";' );
+        header('Content-Length: '.filesize($path));
+
+        readfile($path);
+        die;
+    }
+}
